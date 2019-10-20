734b59f01205e7fcbb8b84c32f07374158e30a6a
James Long <451845431.cl@gmail.com>
2019-10-20 22:41:01 +0200

    (Testing) init database

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 
-app = Flask(__name__,template_folder='./frontend/templates',static_folder='./frontend/static')
-
 from application import routes
 
+from . import models
+from config import Config
 
+app = Flask(__name__,template_folder='./frontend/templates',static_folder='./frontend/static')
+app.config.from_object(Config)
+
+# Connect sqlalchemy to app
+models.db.init_app(app)
+
+@app.cli.command()
+def init_db():
+   models.init_db()
---
+++ application/models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from application import app
+
+import logging as lg
+
+# get variables from __init__.py
+#app.config['SQLALCHEMY_DATABASE_URI']
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    gender = db.Column(db.Integer(), nullable=False)
+
+    def __init__(self, description, gender):
+        self.description = description
+        self.gender = gender
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", 1))
+    db.session.add(Content("What's your favorite scary movie?", 0))
+    db.session.commit()
+    lg.warning('Database initialized!')
